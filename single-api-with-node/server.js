9299422d028d7233e22211e0ccf143cde0217126
Oleg Lipovoi <olipovoi@gmail.com>
2017-08-02 15:13:07 +0300

    Added Express routers.

--- single-api-with-node/server.js
+++ single-api-with-node/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 // Init server
 var app = express();
+
+// Parse JSON
+app.use(bodyParser.json());
+// Parse form date
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var artists = [
     {
@@ -34,6 +40,31 @@ app.get('/artists/:id', function (req, res) {
         return artist.id === Number(req.params.id)
     });
     res.send(artist);
+})
+
+
+app.post('/artists', function (req, res) {
+    var artist = {
+        id: Date.now(),
+        name: req.body.name
+    };
+    artists.push(artist);
+    res.send(artist);
+})
+
+app.put('/artists/:id', function (req, res) {
+    var artist = artists.find(function (artist) {
+        return artist.id === Number(req.params.id)
+    });
+    artist.name = req.body.name;
+    res.sendStatus(200);
+})
+
+app.delete('/artists/:id', function (req, res) {
+    artists = artists.filter(function (artist) {
+        return artist.id !== Number(req.params.id);
+    })
+    res.sendStatus(200);
 })
 
 // Configure server
